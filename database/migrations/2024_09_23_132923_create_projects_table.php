a9610bb8773c6f89ec24b19091136ce81ee00b07
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-23 15:52:16 +0200

    Migration of projects tables and populate with faker

--- database/migrations/2024_09_23_132923_create_projects_table.php
+++ database/migrations/2024_09_23_132923_create_projects_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 100);
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->tinyInteger('collaborators');
+            $table->string('img')->nullable();
             $table->timestamps();
         });
     }
