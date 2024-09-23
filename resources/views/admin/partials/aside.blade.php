e8aafb5cfca6f5df3b8a5be05f3f92a9c11d3801
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-23 17:16:51 +0200

    Build: add create view not working

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -4,10 +4,10 @@
             <a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> HOME</a>
         </li>
         <li>
-            <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list"></i> Elenco post</a>
+            <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list"></i> Elenco progetti</a>
         </li>
         <li>
-            <a href="#"><i class="fa-solid fa-newspaper"></i> Nuovo post</a>
+            <a href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-newspaper"></i> Nuovo progetti</a>
         </li>
     </ul>
 </aside>
